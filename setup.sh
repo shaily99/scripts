0fdb5487b719d08a896c99a02f8d3af773e5f387
shaily99 <shailyjbhatt99@gmail.com>
2023-10-30 14:28:47 -0400

    Updated setup script t

--- setup.sh
+++ setup.sh
@@ -4,11 +4,23 @@
 # Installs regularly used packages.
 # Won't work on login node because it is not good practice to run anything on login node.
 
+while getopts ":n" option; do
+  case $option in
+    n)
+      env_name="$OPTARG"
+      ;;
+    *)
+      echo "Usage: $0 [-f file_name] [-d directory_name]"
+      exit 1
+      ;;
+  esac
+done
+
 if [[ `hostname` != "babel-login-*.lti.cs.cmu.edu" ]]; then
-    conda create -n myenv
-    conda activate myenv
-    conda install -y pytorch::pytorch torchvision torchaudio -c pytorch
+    conda create -n $env_name
+    conda activate $env_name
+    conda install pytorch pytorch-cuda=12.1 -c pytorch -c nvidia
     conda install numpy
-    pip install pandas
-    pip install argparse
+    conda install pandas
+    conda install argparse
 fi
