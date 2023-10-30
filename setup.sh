cfecfab68b9f9f88f7deb43f79f6ec441f769f60
shaily99 <shailyjbhatt99@gmail.com>
2023-10-30 14:31:32 -0400

    Removed flag -- causing issues

--- setup.sh
+++ setup.sh
@@ -4,21 +4,9 @@
 # Installs regularly used packages.
 # Won't work on login node because it is not good practice to run anything on login node.
 
-while getopts ":n" option; do
-  case $option in
-    n)
-      env_name="$OPTARG"
-      ;;
-    *)
-      echo "Usage: $0 [-f file_name] [-d directory_name]"
-      exit 1
-      ;;
-  esac
-done
-
 if [[ `hostname` != "babel-login-*.lti.cs.cmu.edu" ]]; then
-    conda create -n $env_name
-    conda activate $env_name
+    conda create -n myenv
+    conda activate myenv
     conda install pytorch pytorch-cuda=12.1 -c pytorch -c nvidia
     conda install numpy
     conda install pandas
