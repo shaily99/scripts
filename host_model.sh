6304ae5d970914d4f82a7e4099bfcae76b9e630c
shaily99 <shailyjbhatt99@gmail.com>
2024-01-28 14:35:31 -0500

    everything works now.

--- host_model.sh
+++ host_model.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=debug
 #SBATCH --mem=64GB
 #SBATCH -t 0-00:30:00              # time limit: (D-HH:MM) 
-#SBATCH --job-name=quantized-vicuna-7b-noshard-nocache
-#SBATCH --error=logs/quantized-vicuna-7b-noshard-nocache.err
-#SBATCH --output=logs/quantized-vicuna-7b-noshard-nocache.out
+#SBATCH --job-name=vicuna7b-quantized
+#SBATCH --error=logs/vicuna7b-quantized.err
+#SBATCH --output=logs/vicuna7b-quantized.out
 
 mkdir -p /scratch/shailyjb
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -15,8 +15,8 @@ conda activate /data/tir/projects/tir6/general/pfernand/conda/envs/tgi-env-publi
 cd /home/shailyjb/text-generation-inference
 text-generation-launcher \
     --model-id lmsys/vicuna-7b-v1.5 \
-    --port 8080 \
-    --quantize bitsandbytes # Don't pass if you don't want to quantize
-    # --shard-uds-path /scratch/shailyjb \
-    # --huggingface-hub-cache /data/datasets/models/hf_cache
+    --port 8081 \
+    --quantize bitsandbytes \
+    --shard-uds-path /scratch/shailyjb \
+    --huggingface-hub-cache /data/datasets/models/hf_cache
 
