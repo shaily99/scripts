faf6e85d0ebd143299ea22ead7b64fbe235d2194
shaily99 <shailyjbhatt99@gmail.com>
2024-02-07 22:24:42 -0500

    minor

--- host_model.sh
+++ host_model.sh
@@ -2,12 +2,12 @@
 #SBATCH --gres=gpu:A6000:1
 #SBATCH --partition=general
 #SBATCH --mem=64GB
-#SBATCH --time 1-00:00:00              # time limit: (D-HH:MM) 
-#SBATCH --job-name=llama2_7b
-#SBATCH --error=/home/shailyjb/logs/llama2_7b.err
-#SBATCH --output=/home/shailyjb/logs/llama2_7b.out
+#SBATCH --time 0-03:00:00              # time limit: (D-HH:MM) 
+#SBATCH --job-name=llama_chat_7b
+#SBATCH --error=/home/shailyjb/logs/llama_chat_7b.err
+#SBATCH --output=/home/shailyjb/logs/llama_chat_7b.out
 #SBATCH --mail-type=END
-#SBATCH --mail-user=shailyjb@andrew.cmu.edus
+#SBATCH --mail-user=shailyjb@andrew.cmu.edu
 
 mkdir -p /scratch/shailyjb
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -16,13 +16,16 @@ conda activate /data/tir/projects/tir6/general/pfernand/conda/envs/tgi-env-publi
 
 cd /home/shailyjb/text-generation-inference
 
-PORT=8080
+MODEL_ID="meta-llama/Llama-2-7b-chat-hf"
+# MODEL_ID="lmsys/vicuna-7b-v1.5"
+
+PORT=8081
 if ss -tulwn | grep -q ":$PORT "; then
     echo "Port $PORT is already in use. Exiting..."
     exit 1
 else
     text-generation-launcher \
-        --model-id meta-llama/Llama-2-7b-hf \
+        --model-id $MODEL_ID \
         --port $PORT \
         --quantize bitsandbytes \
         --shard-uds-path /scratch/shailyjb \
