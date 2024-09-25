2a4a9d9f6ce74a0d2b6c8201a15ea74ad8b2b59c
Shaily Bhatt <31388649+shaily99@users.noreply.github.com>
2024-09-25 17:29:50 -0400

    Create run_query_vllm.sh

---
+++ llm_inference/run_query_vllm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --job-name=name
+#SBATCH --output=/logs/name.out
+#SBATCH --error=/logs/name.err
+#SBATCH --nodes=1
+#SBATCH --mem=16GB
+#SBATCH --time 0-12:55:00
+#SBATCH --partition=cpu
+#SBATCH --mail-type=END
+#SBATCH --mail-user=shailyjb@andrew.cmu.edu
+#SBATCH --exclude=shire-1-6,inst-0-35,shire-1-10
+
+
+echo $SLURM_JOB_ID
+
+source ~/.bashrc
+conda init bash
+conda activate pc2
+
+
+MAX_TOKENS=50
+
+MODEL_ADDRESS="http://babel-4-23:8081/v1"
+MODEL="google/gemma-2b-it"
+MODEL_NAME="gemma2B_it"
+
+PROMPTS="FILE"
+OUTPUT="FILE"
+
+python /home/shailyjb/intrinsic-bias-metaeval/stereoset_query.py \
+    --prompts="${PROMPTS}" \
+    --output="${OUTPUT}" \
+    --model=${MODEL} \
+    --base_url=${MODEL_ADDRESS} \
+    --max_response_tokens=${MAX_TOKENS} \
+    --requests_per_minute=100 \
+    --num_responses_per_prompt=1
